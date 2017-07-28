64576186bbd9f74d12011a027cf0e6491bd5abee
Adrian Sampson <adrian@radbox.org>
2017-07-28 15:12:05 -0400

    Walk to look for text files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate walkdir;
+
+use walkdir::{WalkDir, DirEntry};
+
+fn is_note(entry: &DirEntry) -> bool {
+    entry.file_type().is_file() &&
+    entry.file_name()
+         .to_str()
+         .map(|s| s.ends_with(".txt"))
+         .unwrap_or(false)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let walker = WalkDir::new(".").into_iter();
+    for entry in walker.filter_map(|e| e.ok()) {
+        if is_note(&entry) {
+            println!("{}", entry.path().display());
+        }
+    }
 }
